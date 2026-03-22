061caad1fdd9254130d081bfdec39ce3966bb77b
Bash Ben <bishrben@gmail.com>
2026-03-22 14:09:49 -0400

    feat(nn): completed territory lstm model, started work on training loop

--- model.py
+++ model.py
@@ -1,23 +1,34 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import math
 
 
 class TerritoryModel(nn.Module):
-  def __init__(self, input_size, hidden_size, num_layers, output_size):
-    super().__init__()
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super().__init__()
 
-    self.num_layers = num_layers
-    self.hidden_size = hidden_size
+        self.input_channels = 2 # Two grids (territory, and balance)
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
 
-    self.lstm = nn.LSTM(input_size, hidden_size, num_layers=num_layers, batch_first=True)
-    self.fl = nn.Linear(hidden_size, output_size)
+        self.cnn = nn.Sequential(
+            nn.Conv2d(self.input_channels, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+        self._num_features = math.pow(input_size, 2) * 64
+        self.lstm = nn.LSTM(self._num_features, self.hidden_size, num_layers=self.num_layers, batch_first=True)
+        self.fl = nn.Linear(self.hidden_size, output_size)
 
-  def forward(self, x, hx = None):
-    # x: (batch_size, sequence_length, input_size)
-    # hx: (h0, c0), No memory features for now
-    if hx is None:
-      hx = (torch.zeros(self.num_layers, self.hidden_size), torch.zeros(self.num_layers, self.hidden_size))
-    out, hx = self.lstm(x, hx)
-    logits = self.fl(out)
-    return logits, hx
+    def forward(self, x: torch.Tensor, hx: tuple):
+        # x: (batch_size, channels, 512, 512)
+        # hx: (h0, c0), initially no memory
+        x = self.cnn(x)
+        x = x.unsqueeze(1)
+        x, hx = self.lstm(x, hx)
+        x = x.squeeze(1)
+        logits = self.fl(x)
+        return logits, hx
--- rl.py
+++ rl.py
@@ -3,10 +3,19 @@ import torch.optim as optim
 
 from model import TerritoryModel
 
+from game.core import Game
+
+# ----- Helpers ------
+def init_hidden(hidden_size, num_layers):
+    return (torch.zeros(num_layers, hidden_size), torch.zeros(num_layers, hidden_size))
+
+game = Game(seed=42)
+player_id = game.add_player()
+
+
 lr = 1e-3
 gamma = 0.995
 
 model = TerritoryModel()
-lr = lr
-gamma = gamma # Discount rate - how much later rewards count now: 0 <= gamma <= 1
 optimizer = optim.Adam(model.parameters(), lr=lr)
+
